99552ab662bd87b2f3b3250ded787092514c1e90
Will <wp20131020ad@gmail.com>
2023-09-20 09:48:56 +0800

    make it better

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'apple benanan key'
 
 
 @app.route("/")
@@ -11,16 +12,19 @@ def index():
 
 @app.route('/homepage')
 def homepage():
-    return render_template('homepage.html')
+    return render_template("homepage.html")
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return "welcome %s" % username
+@app.route("/membersonly")
+def membersonly():
+    return render_template("membersonly.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if "username" in session:
+        username = session["username"]
+        return render_template("membersonly.html")
     if request.method == "POST":
         if request.form.get("register") == "register":
             return redirect(url_for("register"))
@@ -29,20 +33,29 @@ def login():
 
         conn = sqlite3.connect("account.db")
         cursor = conn.cursor()
-
         sqlstr = "select password from members where username='"+username+"'"
         cursor.execute(sqlstr)
         data = cursor.fetchone()
+
         if data == None:
-            return "帳號不存在"
+            login_message = "溫馨提示:用戶不存在,請先註冊"
+            return render_template('login.html', message=login_message)
         else:
             if data[0] == password:
-                return redirect(url_for("profile", username=username))
+                session["username"] = username
+                return redirect(url_for("membersonly"))
             else:
-                return "密碼不正確"
+                login_message = "溫馨提示:密碼不正確,請確認"
+                return render_template('login.html', message=login_message)
 
     else:
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return render_template("login.html")
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -50,25 +63,78 @@ def register():
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
+        name = request.form["name"]
+        phonenumber = request.form["phonenumber"]
+        birthday = request.form["birthday"]
+        address = request.form["address"]
         email = request.form["email"]
 
         conn = sqlite3.connect("account.db")
         cursor = conn.cursor()
-
         sqlstr = "INSERT INTO members('username','password','email') values('" + \
-            username+"','"+password+"','"+email+"')"
+            username+"','"+password+"','"+name+"','"+phonenumber + \
+            "','"+birthday+"','"+address+"','"+email+"')"
         cursor.execute(sqlstr)
         conn.commit()
-
         sqlstr = "select password from members where username='"+username+"'"
         cursor.execute(sqlstr)
         data = cursor.fetchone()
+
         if data == None:
-            return "註冊失敗"
+            login_message = "溫馨提示:註冊失敗"
+            return render_template("register.html", message=login_message)
         else:
-            return "註冊成功"
+            return render_template("membersonly.html")
     else:
         return render_template("register.html")
+
+
+def get_product_by_id(product_id):
+    id = request.form[id]
+    conn = sqlite3.connect('account.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT good_name, price FROM goods WHERE id = '"+id+"'")
+    product = cursor.fetchone()
+    conn.close()
+    return product
+
+
+@app.route('/add_to_cart/<int:product_id>')
+def add_to_cart(product_id):
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    cart = session['cart']
+
+    # Retrieve product data from the SQLite database
+    product = get_product_by_id(product_id)
+
+    if product:
+        if product_id in cart:
+            cart[product_id]['quantity'] += 1
+        else:
+            cart[product_id] = {'name': product[1],
+                                'price': product[2], 'quantity': 1}
+
+    session['cart'] = cart
+    return redirect(url_for('home'))
+
+
+@app.route('/cart')
+def cart():
+    cart = session.get('cart', {})
+    total_price = sum(item['price'] * item['quantity']
+                      for item in cart.values())
+    return render_template('cart.html', cart=cart, total_price=total_price)
+
+
+@app.route('/remove_from_cart/<int:product_id>')
+def remove_from_cart(product_id):
+    cart = session.get('cart', {})
+    if product_id in cart:
+        del cart[product_id]
+        session['cart'] = cart
+    return redirect(url_for('cart'))
 
 
 if __name__ == "__main__":
