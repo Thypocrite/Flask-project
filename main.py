fb284b0485e6cf22de22669083d3ef4dade49c73
Will <wp20131020ad@gmail.com>
2023-10-01 11:11:35 +0800

    'update'

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template, redirect, url_for, session, flash
-import re
-from bs4 import BeautifulSoup
-from datetime import date
 import sqlite3
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from datetime import date
+import time
+import re
+
 
 app = Flask(__name__)
 app.secret_key = 'apple benanan key'
@@ -378,6 +381,201 @@ def clear_cart():
     conn.close()
 
     return redirect('/cart')
+
+
+# 管理區-商品上架
+@app.route('/productsonshelves', methods=['POST', 'GET'])
+def productsonshelves():
+    if request.method == 'POST':
+        conn = sqlite3.connect('merged.db')
+        cur = conn.cursor()
+
+# 建立名為 "products" 的資料表
+        cur.execute('''CREATE TABLE IF NOT EXISTS products
+                        (salePageId INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT,
+                        price TEXT,
+                        image_url TEXT,
+                        link_url TEXT,
+                        first_description TEXT,
+                        second_description TEXT)''')
+
+
+# 创建图像链接表
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS image_urls (
+            salePageId TEXT,
+            url TEXT
+            )
+        ''')
+# 使用 Selenium 開啟網頁
+
+        urls = [
+            'https://www.tvgame.com.tw/v2/official/SalePageCategory/353560?sortMode=Curator',
+            'https://www.tvgame.com.tw/v2/official/SalePageCategory/494728?sortMode=Curator'
+        ]
+
+# 查詢資料庫以獲取已經存在的商品
+        cur.execute("SELECT title FROM products")
+
+        existing_products = [row[0] for row in cur.fetchall()]
+
+# 创建一个名为 "products" 的表格
+
+
+# 遍歷每個URL
+        for url in urls:
+           # 使用 Selenium 開啟網頁
+            driver = webdriver.Chrome()
+            driver.get(url)
+
+
+# 使用 Selenium 等待網頁載入完成（根據實際情況調整等待時間）
+            time.sleep(5)
+
+    # 獲取網頁內容
+            html_content = driver.page_source
+
+    # 關閉網頁
+            driver.quit()
+
+    # 使用 BeautifulSoup 解析網頁內容
+            soup = BeautifulSoup(html_content, 'html.parser')
+
+    # 找到包含商品信息的HTML元素
+            product_elements = soup.find_all(
+                'li', class_='column-grid-container__column')
+
+            first_description = "未提供商品描述"
+            second_description = "未提供商品描述"
+            additional_info_div = None  # 用于存储附加信息的<div>
+            image_urls = []  # 用于存储图片URL的列表
+
+            if product_elements:
+                for product_element in product_elements:
+                    # 從每個商品元素中提取標題
+                    title_element = product_element.find(
+                        'div', class_='sc-ALVnD jttWOh')
+
+                    if title_element:
+                        title = title_element.text.strip()
+               # 如果商品在資料庫中已經存在，從 existing_products 中移除
+                        if title in existing_products:
+                            existing_products.remove(title)
+                        else:
+                            price_element = product_element.find(
+                                'div', class_='sc-KzItE gRTtOW')
+                            image_element = product_element.find(
+                                'img', class_='product-card__vertical__media product-card__vertical__media-tall')
+                            link_element = product_element.find(
+                                'a', class_='sc-bSGrXo dvnSEx product-card__vertical product-card__vertical--hover new-product-card')
+
+                            if price_element and image_element and link_element:
+                                price = price_element.text.strip()
+                                image_url = image_element['src']
+                                link_url = 'https://www.tvgame.com.tw' + \
+                                    link_element['href']
+                                salePageId = link_url.split('/')[-1]
+
+                        # 插入新的商品信息到資料庫中
+                        # cur.execute("INSERT INTO products (title, price, image_url, link_url) VALUES (?, ?, ?, ?)",
+                        #             (title, price, image_url, link_url))
+                        # conn.commit()
+
+                        # 輸出提取的商品信息
+                                print(f'標題: {title}')
+                                print(f'價格: {price}')
+                                print(f'圖片 URL: {image_url}')
+                                print(f'連結 URL: {link_url}')
+
+                        # 進入商品詳細頁面
+                                driver = webdriver.Chrome()
+                                driver.get(link_url)
+
+                        # 在商品詳細頁面上進行更多信息提取
+                        # 這裡可以添加代碼来提取更多细信息
+                        # 使用 Selenium 等待商品詳細頁面載入完成（根據實際情況調整等待時間）
+                                time.sleep(5)
+
+                                page_source = driver.page_source
+                                soup = BeautifulSoup(
+                                    page_source, 'html.parser')
+
+                                product_info = soup.find(
+                                    "div", class_="right-sub-content")
+
+                                first_description = "未提供商品描述"
+                                second_description = "未提供商品描述"
+                                additional_info_div = None  # 用于存储附加信息的<div>
+                                image_urls = []  # 用于存储图片URL的列表
+
+                                if product_info:
+                                    # 查找包含商品描述的HTML元素，这里假设商品描述位于一个<p>元素中
+                                    product_description_span = product_info.find(
+                                        "div", class_="salepage-detail-info")
+
+                                    if product_description_span:
+                                        product_description = product_description_span.text.strip()
+
+                                # 删除特定文本
+                                        specific_text = "※無法指定收貨時間，有特殊需求請先至客服詢問 (訂單備註非問答區)※如遇原廠貨量不足/取消生產/調整價格/延遲出貨，將於收到原廠發布消息後以簡訊通知取消訂單/重新下單。(請定時確認帳號電話號碼正確&暢通，若設定不接收商務簡訊導致未收到通知請自行至商品頁或客服留言確認資訊)※請於上市前檢查取貨門市是否閉店、閉店將無法出貨。重選後也請於客服留言告知可出貨。"
+                                        product_description = product_description.replace(
+                                            specific_text, "")
+                                        first_description = specific_text
+                                        second_description = product_description
+
+                             # 在<div>元素中查找<img>元素
+                                    img_elements = product_info.find_all("img")
+
+                                    if img_elements:
+                                        # 提取<img>元素的src属性，即图片的URL
+                                        for img_element in img_elements:
+                                            image_url2 = img_element.get("src")
+                                            if image_url2:
+                                                image_urls.append(image_url2)
+
+                         # 将数据插入产品表
+                                cur.execute('''
+                                        INSERT INTO products (salePageId, title, price, image_url, link_url, first_description, second_description)
+                                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                                    ''', (salePageId, title, price, image_url, link_url, first_description, second_description))
+
+                        # 将图像链接插入图像链接表
+                                for url in image_urls:
+                                    if "webapi/images/r/SalePageDesc" in url:
+                                        cur.execute('''
+                                            INSERT INTO image_urls (salePageId, url)
+                                            VALUES (?, ?)
+                                        ''', (salePageId, url))
+
+                                conn.commit()
+
+                                print(f'標題: {title}')
+                                print(f'價格: {price}')
+                                print(f'圖片 URL: {image_url}')
+                                print(f'連結 URL: {link_url}')
+                                print(f"商品描述: {product_description}")
+                                print(f"第一段商品描述: {first_description}")
+                                print(f"第二段商品描述: {second_description}")
+                                for url in image_urls:
+                                    if "webapi/images/r/SalePageDesc" in url:
+                                        print(f"销售页面图像链接: {url}")
+                        # 詳關閉商品詳細頁面
+                                driver.quit()
+
+
+# 刪除不再存在於網站上的商品記錄
+        if existing_products:
+            for product_title in existing_products:
+                cur.execute("DELETE FROM products WHERE title=?",
+                            (product_title,))
+            conn.commit()
+
+# 關閉資料庫連接
+        conn.close()
+
+    else:
+        return render_template('productsonshelves.html')
 
 
 if __name__ == "__main__":
