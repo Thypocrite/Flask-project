682f34a6ffd89f76cb20494b6974e9e8ce08a8f3
Will <wp20131020ad@gmail.com>
2023-10-07 07:51:34 +0800

    checkoutPage'

--- static/js/getCurrentURL.js
+++
@@ -1,5 +0,0 @@
-// Get the current URL using JavaScript
-const currentUrl = window.location.href;
-// Set the value of the hidden input field
-
-document.getElementById('current-url').value = currentUrl;
